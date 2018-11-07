7b9ac38f17b3e2767b86ebff406e7bce1a98f617
Xingyu Yang <yangxingyu18@sdjzu.edu.cn>
2018-11-07 21:11:36 +0800

    增加了一个扫雷程序

---
+++ MineWweeping/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace MineWweeping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Xbox = new System.Windows.Forms.TextBox();
+            this.Ybox = new System.Windows.Forms.TextBox();
+            this.LSbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(37, 17);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Xbox
+            // 
+            this.Xbox.Location = new System.Drawing.Point(176, 38);
+            this.Xbox.Margin = new System.Windows.Forms.Padding(4);
+            this.Xbox.Name = "Xbox";
+            this.Xbox.Size = new System.Drawing.Size(46, 28);
+            this.Xbox.TabIndex = 1;
+            this.Xbox.Text = "10";
+            // 
+            // Ybox
+            // 
+            this.Ybox.Location = new System.Drawing.Point(267, 38);
+            this.Ybox.Margin = new System.Windows.Forms.Padding(4);
+            this.Ybox.Name = "Ybox";
+            this.Ybox.Size = new System.Drawing.Size(46, 28);
+            this.Ybox.TabIndex = 2;
+            this.Ybox.Text = "10";
+            // 
+            // LSbox
+            // 
+            this.LSbox.Location = new System.Drawing.Point(376, 38);
+            this.LSbox.Margin = new System.Windows.Forms.Padding(4);
+            this.LSbox.Name = "LSbox";
+            this.LSbox.Size = new System.Drawing.Size(46, 28);
+            this.LSbox.TabIndex = 3;
+            this.LSbox.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(141, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "列";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(324, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "雷数";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 42);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "行";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 542);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LSbox);
+            this.Controls.Add(this.Ybox);
+            this.Controls.Add(this.Xbox);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "扫雷 DesignBy杨兴宇";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Xbox;
+        private System.Windows.Forms.TextBox Ybox;
+        private System.Windows.Forms.TextBox LSbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MineWweeping/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace MineWweeping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        public mine saolei = new mine();
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static short x, y, ls;
+        private void Bt_MouseDown(object sender, MouseEventArgs e)
+        {
+            Button bt = (Button)sender;
+            Point qp = new Point((bt.Location.X - 30) / 25, (bt.Location.Y - 80) / 25);
+            if (e.Button == MouseButtons.Left)//判断左右键
+            {
+                if (saolei.MineMap[qp.X, qp.Y] == 1)
+                {
+                    MessageBox.Show("BOOM!");
+                    //踩雷之后显示所有的雷
+                    saolei.Showall(x, y);
+                }
+                else
+                {
+                    saolei.play(qp.X, qp.Y);
+
+                }
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                if (bt.Text.Equals("?"))
+                {
+                    bt.BackColor = Color.DarkGray;
+                    bt.Text = "";
+                    saolei.JiLu[qp.X, qp.Y] = ' ';
+                    saolei.leishu--;
+                }
+                else
+                {
+                    bt.BackColor = Color.Yellow;
+                    bt.Text = "?";
+                    saolei.JiLu[qp.X, qp.Y] = '?';
+                    saolei.leishu++;
+                }
+            }
+            //输出变化
+            saolei.Flash(x, y);
+            if (saolei.leishu == ls)
+            {
+                if (saolei.Checkwin(x, y, saolei.leishu))
+                {
+                    MessageBox.Show("Yeeeeeep!");
+                    saolei.Showall(x, y);
+                }
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Button lab in this.Controls.OfType<Button>())
+            {
+                lab.Visible = false;
+            }
+            button1.Visible = true;
+            x = short.Parse(Xbox.Text.ToString());
+            y = short.Parse(Ybox.Text.ToString());
+            ls = short.Parse(LSbox.Text.ToString());
+            if (x > 10 || y > 10)
+            {
+                this.Size = new Size(25 * x + 100, 25 * y + 150);
+            }
+            saolei.build(x, y, ls);
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Button bt = new Button();
+                    bt.Location = new System.Drawing.Point(30 + 25 * i, 80 + 25 * j);
+                    bt.Size = new Size(25, 25);
+                    bt.BackColor = Color.Gray;
+                    bt.Text = "";
+                    //调试显示答案用
+                    //if (saolei.MineMap[i, j] == 1)
+                    //{
+                    //    bt.Text = "";
+                    //}
+                    //else
+                    //{
+                    //    bt.Text = "";// saolei.JiLu[i,j].ToString();
+                    //}
+
+                    bt.TextAlign = ContentAlignment.MiddleCenter;
+                    //bt.Click += new System.EventHandler(Bt_Click);
+                    bt.MouseDown += new MouseEventHandler(Bt_MouseDown);
+                    saolei.buttons[i, j] = bt;
+                    this.Controls.Add(bt);
+                }
+            }
+        }
+
+        public class mine
+        {
+            public int[,] MineMap;
+            public Char[,] JiLu;
+            public Button[,] buttons;
+            public int leishu = 0;
+            //建立地雷块
+            public void build(int x, int y, int leishu)
+            {
+                long tick = DateTime.Now.Ticks;
+                Random ran = new Random((int)(tick & 0xffffffffL) | (int)(tick >> 32));
+
+                if (x * y <= leishu)
+                {
+                    MessageBox.Show("地雷过多!");
+                    return;
+                }
+                MineMap = new int[x, y];
+                JiLu = new char[x, y];
+                buttons = new Button[x, y];
+                int[] num = new int[x * y];
+                for (int i = 0; i < leishu; i++)
+                {
+                    num[i] = 1;
+                }
+                ArrayList list = new ArrayList();
+                for (int i = 0; i < x * y; i++)
+                {
+                    int number = ran.Next(0, x * y);
+                    while (list.Contains(number))
+                    {
+                        number = ran.Next(0, x * y);
+                    }
+                    list.Add(number);
+                }
+
+                int jishu = 0;
+                for (int i = 0; i < x; i++)
+                {
+                    for (int j = 0; j < y; j++)
+                    {
+                        MineMap[i, j] = num[(int)list[jishu++]];
+                    }
+                }
+                for (int i = 0; i < x; i++)
+                {
+                    for (int j = 0; j < y; j++)
+                    {
+                        JiLu[i, j] = '*';
+                    }
+                }
+            }
+            //寻找点周边雷的个数
+            public int find(Point qp)
+            {
+                int jishu = 0;
+                for (int i = -1; i < 2; i++)
+                {
+                    for (int j = -1; j < 2; j++)
+                    {
+                        if (i == 0 && j == 0)
+                        {
+                            continue;
+                        }
+                        if ((qp.X + i >= 0) && (qp.X + i < Form1.x) && (qp.Y + j >= 0) && (qp.Y + j < Form1.y))
+                        {
+                            if (MineMap[qp.X + i, qp.Y + j] == 1)
+                            {
+                                jishu++;
+                            }
+                        }
+                    }
+                }
+                return jishu;
+            }
+            //递归炸开块
+            public void play(int x, int y)
+            {
+                int ret;
+                Point p = new Point();
+                p.X = x; p.Y = y;
+                ret = find(p);
+                if (ret == 0)
+                {
+                    JiLu[x, y] = ' ';
+                    if ((x - 1) >= 0 && (y - 1) >= 0 && (x - 1) < Form1.x && (y - 1) < Form1.y && (JiLu[x - 1, y - 1] == '*'))
+                        play(x - 1, y - 1);
+                    if ((x - 1) >= 0 && (y) >= 0 && (x - 1) < Form1.x && (y) < Form1.y && (JiLu[x - 1, y] == '*'))
+                        play(x - 1, y);
+                    if ((x - 1) >= 0 && (y + 1) >= 0 && (x - 1) < Form1.x && (y + 1) < Form1.y && (JiLu[x - 1, y + 1] == '*'))
+                        play(x - 1, y + 1);
+                    if ((x) >= 0 && (y - 1) >= 0 && (x) < Form1.x && (y - 1) < Form1.y && (JiLu[x, y - 1] == '*'))
+                        play(x, y - 1);
+                    if ((x) >= 0 && (y + 1) >= 0 && (x) < Form1.x && (y + 1) < Form1.y && (JiLu[x, y + 1] == '*'))
+                        play(x, y + 1);
+                    if ((x + 1) >= 0 && (y - 1) >= 0 && (x + 1) < Form1.x && (y - 1) < Form1.y && (JiLu[x + 1, y - 1] == '*'))
+                        play(x + 1, y - 1);
+                    if ((x + 1) >= 0 && (y) >= 0 && (x + 1) < Form1.x && (y) < Form1.y && (JiLu[x + 1, y] == '*'))
+                        play(x + 1, y);
+                    if ((x + 1) >= 0 && (y + 1) >= 0 && (x + 1) < Form1.x && (y + 1) < Form1.y && (JiLu[x + 1, y + 1] == '*'))
+                        play(x + 1, y + 1);
+                }
+                else
+                {
+                    JiLu[x, y] = (char)(ret + '0');
+                }
+            }
+            //显示所有雷
+            public void Showall(int x, int y)
+            {
+                for (int i = 0; i < x; i++)
+                {
+                    for (int j = 0; j < y; j++)
+                    {
+                        if (MineMap[i, j] == 1)
+                        {
+                            buttons[i, j].Text = "◈";
+                            buttons[i, j].BackColor = Color.Red;
+                        }
+                    }
+                }
+            }
+            //胜利判定
+            public bool Checkwin(int x, int y, int jishu)
+            {
+                int js = jishu;
+                for (int i = 0; i < x; i++)
+                {
+                    for (int j = 0; j < y; j++)
+                    {
+                        if (JiLu[i, j] == '?' && MineMap[i, j] == 1)
+                        {
+                            js--;
+                        }
+
+                    }
+                }
+                if (js == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            //刷新
+            public void Flash(int x, int y)
+            {
+                for (int i = 0; i < x; i++)
+                {
+                    for (int j = 0; j < y; j++)
+                    {
+                        if (JiLu[i, j] != '*')
+                        {
+                            if (JiLu[i,j]=='?')
+                            {
+                                buttons[i, j].BackColor = Color.Yellow;
+                            }
+                            else
+                            {
+                                buttons[i, j].BackColor = Color.DarkGray;
+                            }
+                            buttons[i, j].Text = JiLu[i, j].ToString();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
